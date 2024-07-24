2166efb6861af0bbd549fea5ccc9aaa45d6eb86b
zelshenhab <z.elshenhab2012@gmail.com>
2024-07-25 00:09:54 +0300

    custom app bar to home

---
+++ lib/Features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bookly/core/utils/assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 60),
+      child: Row(
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 18,
+          ),
+          const Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search, size: 30),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bookly/Features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:flutter_bookly/core/utils/assets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [CustomAppBar()],
+    );
+  }
+}
