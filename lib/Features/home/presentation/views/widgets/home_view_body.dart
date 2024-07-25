6ce1194879a0da49067f0e53532200f40332b34f
zelshenhab <z.elshenhab2012@gmail.com>
2024-07-25 23:51:59 +0300

    create styles file and add title to home view

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -17,22 +17,3 @@ class HomeViewBody extends StatelessWidget {
     );
   }
 }
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: CustomLisyViewItem(),
-            );
-          }),
-    );
-  }
-}
