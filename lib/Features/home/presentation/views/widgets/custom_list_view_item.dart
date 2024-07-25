8fb3e93328e0c4931e7043d90c6fdbce3a34ec67
zelshenhab <z.elshenhab2012@gmail.com>
2024-07-25 23:52:39 +0300

    create styles file and add title to home

--- lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -2,8 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bookly/core/utils/assets.dart';
 
-class CustomLisyViewItem extends StatelessWidget {
-  const CustomLisyViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bookly/Features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:flutter_bookly/Features/home/presentation/views/widgets/custom_list_view_item.dart';
-import 'package:flutter_bookly/core/utils/assets.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_bookly/Features/home/presentation/views/widgets/featured_list_view.dart';
+import 'package:flutter_bookly/core/utils/styles.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            "Best Seller",
+            style: Styles.titleMedium,
+          )
+        ],
+      ),
     );
   }
 }
