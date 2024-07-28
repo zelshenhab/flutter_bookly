a593befaa715d8cca38de4ea03059dcaf443cd2a
zelshenhab <z.elshenhab2012@gmail.com>
2024-07-28 18:36:11 +0300

    create styles

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bookly/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter_bookly/Features/home/presentation/views/widgets/featured_list_view.dart';
+import 'package:flutter_bookly/core/utils/assets.dart';
 import 'package:flutter_bookly/core/utils/styles.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -20,7 +21,37 @@ class HomeViewBody extends StatelessWidget {
           ),
           Text(
             "Best Seller",
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+                decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: const DecorationImage(
+                  image: AssetImage(
+                AssetsData.testImage,
+              )),
+            )),
+          ),
+          Column(
+            children: [],
           )
         ],
       ),
