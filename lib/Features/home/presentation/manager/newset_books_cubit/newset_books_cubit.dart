f2595f266f9446c7176d94580563c9b56e7dd2cb
Zeyad Elshenhab <z.elshenhab2012@gmail.com>
2024-08-07 20:08:47 +0300

    display featured books part 1

--- lib/Features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bookly/Features/home/data/models/book_model/book_model.dart';
 import 'package:flutter_bookly/Features/home/data/repos/home_repo.dart';
 
---
+++ lib/core/widgets/custom_error_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bookly/core/utils/styles.dart';
+
+class CustomErrorWidget extends StatelessWidget {
+  const CustomErrorWidget({super.key, required this.errMessage});
+  final String errMessage;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      errMessage,
+      style: Styles.textStyle18,
+    );
+  }
+}
