757bb2c8cd725ba73cb6aa4ae925cf62045e6305
Zeyad Elshenhab <z.elshenhab2012@gmail.com>
2024-08-11 18:33:31 +0300

    lanuch url part 2 done

--- lib/Features/home/presentation/views/widgets/books_action.dart
+++ lib/Features/home/presentation/views/widgets/books_action.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:flutter_bookly/core/utils/function/launch_url.dart';
 import 'package:flutter_bookly/core/widgets/custom_button.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class BooksAction extends StatelessWidget {
   const BooksAction({super.key, required this.bookModel});
@@ -23,12 +23,11 @@ class BooksAction extends StatelessWidget {
             ),
           ),
           CustomButton(
-              onPressed: () async {
-                Uri uri = Uri.parse(bookModel.volumeInfo.previewLink!);
-                if (!await launchUrl(uri)) {}
+              onPressed: () {
+                launchCustomUrl(context, bookModel.volumeInfo.previewLink);
               },
               fontSize: 16,
-              text: "Preview",
+              text: getText(bookModel),
               backgroundColor: const Color(0xffEF8262),
               textColor: Colors.white,
               borderRadius: const BorderRadius.only(
@@ -38,5 +37,13 @@ class BooksAction extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  String getText(BookModel bookModel) {
+    if (bookModel.volumeInfo.previewLink == null) {
+      return 'Not Avaliable';
+    } else {
+      return "Preview";
+    }
   }
 }
